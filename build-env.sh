b0887950bca7feccd3087f24be8fcfd7b589e212
hrodmn <henry.rodman@gmail.com>
2025-04-25 11:51:44 -0500

    install curl if not available

--- build-env.sh
+++ build-env.sh
@@ -2,6 +2,12 @@
 set -euo pipefail
 # This script is used to install any custom packages required by the algorithm.
 
+# install curl if not available
+command -v curl >/dev/null 2>&1 || { 
+    echo "Installing curl..."
+    apt-get update && apt-get install -y curl
+}
+
 # install uv
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
